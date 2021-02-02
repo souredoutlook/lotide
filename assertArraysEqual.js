4dbee097eee28c8abb2761e260806e93b648b789
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-02 21:43:41 +0000

    Added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const passEmoji = String.fromCodePoint(0x1F49A);
+const failEmoji = String.fromCodePoint(0x1F198);
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`${passEmoji}${passEmoji}${passEmoji} Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`${failEmoji}${failEmoji}${failEmoji} Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++){
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+// TEST CODE
+
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [3, 2, 1]);
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+// assertArraysEqual([], ["1", "2", 3]);
+// assertArraysEqual([], []);
