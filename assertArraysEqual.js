acdd548b0b317a8ba79aeee9190634218c2f5eb9
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-08 19:10:29 +0000

    refactored assertArraysEqual.js, added test/assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays')
+
 const passEmoji = String.fromCodePoint(0x1F49A);
 const failEmoji = String.fromCodePoint(0x1F198);
 
@@ -9,23 +11,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
-};
-
-// TEST CODE
-
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 3], [3, 2, 1]);
-// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-// assertArraysEqual([], ["1", "2", 3]);
-// assertArraysEqual([], []);
+module.exports = assertArraysEqual;
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,11 +1,10 @@
-const assertArraysEqual
+const assertArraysEqual = require('../assertArraysEqual');
 
 // TEST CODE
 
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);// => true
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-// assertEqual(eqArrays([], ["1", "2", 3]), false);
-// assertEqual(eqArrays([], []), true);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([], ["1", "2", 3]);
+assertArraysEqual([], []);
