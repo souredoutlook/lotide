7dfb6ad8dd6ee6e2e6a9b810aa5209935ba856b0
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-08 21:13:10 +0000

    refactored midddle.js, added test/middleTest.js

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const passEmoji = String.fromCodePoint(0x1F49A);
-const failEmoji = String.fromCodePoint(0x1F198);
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`${passEmoji}${passEmoji}${passEmoji} Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`${failEmoji}${failEmoji}${failEmoji} Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
-};
-
 const middle = function(wholeArray) {
   if (Array.isArray(wholeArray)) {
     if (wholeArray.length > 2){
@@ -31,11 +8,5 @@ const middle = function(wholeArray) {
     }
   }
 };
-// TEST CODE
 
-// assertArraysEqual(middle([1]),[]);
-// assertArraysEqual(middle([1, 2]),[]);
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
