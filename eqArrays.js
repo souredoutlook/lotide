6f2628fffb43c8e6322437ec538bc281f580b52a
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-08 19:03:43 +0000

    refactored eqArrays, added test/eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,3 @@
-const passEmoji = String.fromCodePoint(0x1F49A);
-const failEmoji = String.fromCodePoint(0x1F198);
-
-const assertEqual = function(actual, expected) {
-  if (typeof(actual) === "string") {
-    actual = `"${actual}"`;
-  }
-  if (typeof(expected) === "string") {
-    expected = `"${expected}"`;
-  }
-  if (actual === expected) {
-    console.log(`${passEmoji}${passEmoji}${passEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failEmoji}${failEmoji}${failEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++){
@@ -27,12 +10,4 @@ const eqArrays = function(array1, array2) {
   return false;
 };
 
-// TEST CODE
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);// => true
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-// assertEqual(eqArrays([], ["1", "2", 3]), false);
-// assertEqual(eqArrays([], []), true);
+module.exports = eqArrays;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,11 @@
+const assertArraysEqual
+
+// TEST CODE
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);// => true
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+// assertEqual(eqArrays([], ["1", "2", 3]), false);
+// assertEqual(eqArrays([], []), true);
