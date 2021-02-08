12cfe41713d0f4f7115893fb4337a4a66f3e9bd6
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-08 18:22:46 +0000

    Refactored head.js, assertEqual.js and created test/ directory

---
+++ test/assertEqualTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual')
+
+// TEST CODE
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Foo", "Foo");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual(true, true);
+assertEqual(true, false);
+assertEqual(1, "Foo");
+assertEqual("1", "Foo");
