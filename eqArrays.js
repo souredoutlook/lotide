d9992ea6694dd5036fd420a8e349c5c801bfa0a0
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-15 14:50:10 +0000

    refactored eqArrays to use recursion and work with arrays of various depths.

--- eqArrays.js
+++ eqArrays.js
@@ -2,12 +2,24 @@ const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++){
       if (array1[i] !== array2[i]) {
-        return false;
+        if (array1[i] instanceof Array && array2[i] instanceof Array){
+          if (!eqArrays(array1[i], array2[i])) {
+            return false
+          }
+        } else {
+          return false;
+        }
       }
     }
     return true;
   }
   return false;
 };
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+
 
 module.exports = eqArrays;
